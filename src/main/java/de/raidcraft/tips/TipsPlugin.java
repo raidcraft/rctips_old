e8c2f521010964a9882a137d12594f5b0e61b0ae
mdoering <mdoering@interhyp.de>
2014-06-25 11:02:18 +0200

    intial commit

---
+++ src/main/java/de/raidcraft/tips/TipsPlugin.java
@@ -0,0 +1,19 @@
+package de.raidcraft.tips;
+
+import de.raidcraft.api.BasePlugin;
+
+/**
+ * @author mdoering
+ */
+public class TipsPlugin extends BasePlugin {
+
+    @Override
+    public void enable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void disable() {
+        //TODO: implement
+    }
+}
